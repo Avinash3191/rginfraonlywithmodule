a57010515565319c6f9f0964edac188157644282
Avinash3191 <avinash.miet2008@gmail.com>
2025-08-23 21:02:08 +0530

    Rg name changed from rg-vm to rg-gkp1

--- parentmodule/main.tf
+++ parentmodule/main.tf
@@ -1,6 +1,6 @@
 module "rg_name" {
   source                  = "../childmodule/resource_group"
-  resource_group_name     = "rg-vm"
+  resource_group_name     = "rg-gkp1"
   resource_group_location = "westus"
 }
 
